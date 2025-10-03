cd137af1487c2b91d217cca16b6d720f09039643
“Yuriy <dolgih.yv@gmail.com>
2025-10-03 14:34:05 +0200

    add dependencies to pom, edit City.java Forecast.java, User.java

--- src/main/java/com/weatherforecast/entity/City.java
+++ src/main/java/com/weatherforecast/entity/City.java
@@ -1,9 +1,34 @@
 package com.weatherforecast.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class City {
 
-    Long id;
-    String name;
-    String longitude;
-    String latitude;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String name;
+
+    @NotBlank
+    private String longitude;
+
+    @NotBlank
+    private String latitude;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
 }
--- src/main/java/com/weatherforecast/entity/User.java
+++ src/main/java/com/weatherforecast/entity/User.java
@@ -1,24 +1,54 @@
 package com.weatherforecast.entity;
 
-import java.time.LocalDate;
-import java.util.List;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
 public class User {
 
     public enum Role {
         ADMIN,
         USER,
     }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 15)
+    private String name;
+
+    @Email(regexp = "^[A-Za-z0-9]+@[A-Za-z0-9]+\\.[A-Za-z]{2,}$", message = "Invalid email")
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotBlank
+    private String hashPassword;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
 
-    Long id;
-    String name;
-    String email;
-    String hashPassword;
-    Role role;
-    LocalDate createDate;
-    LocalDate updateDate;
-    List<City> favoriteCities;
+    private LocalDate createDate;
 
+    private LocalDate updateDate;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private Set<City> cities = new HashSet<>();
 }

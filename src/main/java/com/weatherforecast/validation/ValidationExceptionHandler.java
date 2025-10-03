faf33362804a2e4a2b2fe3a85eef3419be2c3379
SergeyGrechikhin <blackblock456@gmail.com>
2025-10-03 18:56:04 +0200

    validation + exception

---
+++ src/main/java/com/weatherforecast/validation/ValidationExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.weatherforecast.validation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class ValidationExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorsDto> handlerValidationException(MethodArgumentNotValidException e) {
+        List<ValidationErrorDto> validationErrors = new ArrayList<>();
+
+        List<ObjectError> objectErrors = e.getBindingResult().getAllErrors();
+
+        for (ObjectError currentError : objectErrors) {
+
+            FieldError fieldError = (FieldError) currentError;
+
+            ValidationErrorDto errorDto = ValidationErrorDto.builder()
+                    .field(fieldError.getField())
+                    .message(fieldError.getDefaultMessage())
+                    .build();
+
+            if (fieldError.getRejectedValue() != null) {
+                errorDto.setRejectedValue(fieldError.getRejectedValue().toString());
+            }
+
+            validationErrors.add(errorDto);
+        }
+
+        return ResponseEntity
+                .badRequest()
+                .body(ValidationErrorsDto.builder()
+                        .errors(validationErrors)
+                        .build());
+    }
+}

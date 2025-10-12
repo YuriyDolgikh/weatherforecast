35be613b72462b0e92ce34050c0718cfbb1fc227
“Yuriy <dolgih.yv@gmail.com>
2025-10-12 16:12:50 +0200

    add getting weather for favourite sities for today

--- src/main/java/com/weatherforecast/exception/AlreadyExistException.java
+++ src/main/java/com/weatherforecast/exception/AlreadyExistException.java
@@ -1,7 +1,6 @@
 package com.weatherforecast.exception;
 
 import com.fasterxml.jackson.databind.RuntimeJsonMappingException;
-import com.fasterxml.jackson.databind.deser.std.StringArrayDeserializer;
 
 public class AlreadyExistException extends RuntimeJsonMappingException {
     public AlreadyExistException(String message) {

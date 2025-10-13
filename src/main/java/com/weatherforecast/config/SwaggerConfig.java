a9edd6f7e132bea0edad9ecb2e5f9518f0dd7ab6
“Yuriy <dolgih.yv@gmail.com>
2025-10-13 13:11:24 +0200

    add swagger

--- src/main/java/com/weatherforecast/config/SwaggerConfig.java
+++ src/main/java/com/weatherforecast/config/SwaggerConfig.java
@@ -2,9 +2,11 @@ package com.weatherforecast.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -12,12 +14,15 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local Server")
+                ))
                 .info(new Info()
                         .title("Weather Forecast API")
                         .description("API for getting weather forecast")
-                        .version("1.0.0")
-                        .contact(new Contact()
-                                .name("Group 05052-m-be")
-                                .email("project05052025@gmail.com")));
+                        .version("v1.0")
+                        .contact(new io.swagger.v3.oas.models.info.Contact()
+                                .name("Group 050525-m-be")
+                                .url("https://github.com/YuriyDolgikh/weatherforecast")));
     }
 }

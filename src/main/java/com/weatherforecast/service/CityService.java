393896d83b638b0b7b297d0294d7f05fe5fc5af7
SergeyGrechikhin <blackblock456@gmail.com>
2025-10-13 12:07:18 +0200

    JAVADOC comments for CityService .

--- src/main/java/com/weatherforecast/service/CityService.java
+++ src/main/java/com/weatherforecast/service/CityService.java
@@ -24,6 +24,7 @@ public class CityService implements CityServiceInterface {
 
     /**
      * Get all cities from database
+     *
      * @return List of CityResponseDto objects
      */
     @Override
@@ -33,6 +34,7 @@ public class CityService implements CityServiceInterface {
 
     /**
      * Get all cities from database with full details
+     *
      * @return List of City objects
      */
     public List<City> getAllCitiesFullDetails() {
@@ -40,9 +42,13 @@ public class CityService implements CityServiceInterface {
     }
 
     /**
-     * Get city by name from the database
-     * @param cityName
-     * @return CityResponseDto object
+     * Retrieves a city from the database by its name.
+     * If a city with the given name does not exist, a {@link NotFoundException}
+     * will be thrown.
+     *
+     * @param cityName the name of the city to retrieve (case-sensitive)
+     * @return a {@link CityResponseDto} containing information about the found city
+     * @throws NotFoundException if the city with the specified name is not found in the database
      */
     @Override
     public CityResponseDto getCityByName(String cityName) {
@@ -51,17 +57,30 @@ public class CityService implements CityServiceInterface {
         return cityConverter.toDto(city);
     }
 
+
     @Override
     public List<CityResponseDto> getCitiesByNameContainsIgnoreCase(String cityName) {
         List<City> cities = cityRepository.findByNameContainingIgnoreCase(cityName);
         return cityConverter.toDtos(cities);
     }
 
+    /**
+     * Retrieves all cities currently added to the favorites of the logged-in user.
+     *
+     * @return a list of {@link CityResponseDto} objects representing the user's favorite cities
+     */
     public List<CityResponseDto> getCitiesByCurrentUser() {
         User user = userService.getCurrentUser();
         return cityConverter.toDtos(user.getCities());
     }
 
+    /**
+     * Adds the specified city to the list of favorite cities of the currently logged-in user.
+     *
+     * @param cityName the name of the city to add to favorites
+     * @return an updated list of {@link CityResponseDto} representing the user's favorite cities
+     * @throws NotFoundException if the city with the specified name is not found in the database
+     */
     @Transactional
     @Override
     public List<CityResponseDto> addCityToFavorite(String cityName) {
@@ -74,6 +93,13 @@ public class CityService implements CityServiceInterface {
         return cityConverter.toDtos(user.getCities());
     }
 
+    /**
+     * Removes the specified city from the list of favorite cities of the currently logged-in user.
+     *
+     * @param cityName the name of the city to remove from favorites
+     * @return an updated list of {@link CityResponseDto} representing the user's favorite cities
+     * @throws NotFoundException if the city with the specified name is not found in the database
+     */
     @Transactional
     @Override
     public List<CityResponseDto> deleteCityFromFavorite(String cityName) {
@@ -89,6 +115,11 @@ public class CityService implements CityServiceInterface {
         return cityConverter.toDtos(user.getCities());
     }
 
+    /**
+     * Saves a new city in the database if it does not already exist.
+     *
+     * @param cityName the name of the city to save
+     */
     @Override
     public void saveCity(String cityName) {
         Optional<City> city = cityRepository.findByName(cityName);

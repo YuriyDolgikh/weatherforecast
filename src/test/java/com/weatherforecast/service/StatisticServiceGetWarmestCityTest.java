d4bfe351bf97ae6c3d4001637d850d599c777ea9
SergeyGrechikhin <blackblock456@gmail.com>
2025-10-15 16:46:11 +0200

    test StatisticService

--- src/test/java/com/weatherforecast/service/StatisticServiceGetWarmestCityTest.java
+++ src/test/java/com/weatherforecast/service/StatisticServiceGetWarmestCityTest.java
@@ -2,14 +2,11 @@ package com.weatherforecast.service;
 
 import com.weatherforecast.dto.city.CityResponseDto;
 import com.weatherforecast.dto.forecast.ForecastRequestDto;
-import com.weatherforecast.dto.user.UserResponseDto;
-import com.weatherforecast.entity.City;
-import com.weatherforecast.entity.Forecast;
 import com.weatherforecast.entity.User;
 import com.weatherforecast.exception.NotFoundException;
 import com.weatherforecast.repository.CityRepository;
+import com.weatherforecast.repository.ForecastRepository;
 import com.weatherforecast.repository.UserRepository;
-import com.weatherforecast.service.util.CityConverter;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,23 +17,19 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestPropertySource(locations = "classpath:application-test.yml")
-class StatisticServiceGetAllFavoritesCitiesTest {
+class StatisticServiceGetWarmestCityTest {
     @MockBean
     private CommandLineRunner lineRunner;
 
@@ -58,10 +51,14 @@ class StatisticServiceGetAllFavoritesCitiesTest {
     @Autowired
     private ForecastService forecastService;
 
+    @Autowired
+    private ForecastRepository forecastRepository;
+
     @AfterEach
     void tearDown() {
         userRepository.deleteAll();
         cityRepository.deleteAll();
+        forecastRepository.deleteAll();
     }
 
     @BeforeEach
@@ -78,46 +75,43 @@ class StatisticServiceGetAllFavoritesCitiesTest {
                 .build();
 
         userRepository.save(user1);
+
     }
-
-
 
     @BeforeEach
-    void getForecast() {
-        ForecastRequestDto request = new ForecastRequestDto("Berlin");
-        forecastService.get7DayForecast(request);
-    }
-
-
-    @Test
-    void getAllCitiesInFavorites_whenNoUsers_throws() {
-        userRepository.deleteAll();
-
-        assertThrows(NotFoundException.class,
-                () -> statisticService.getAllCitiesInFavorites());
+    void createCity() {
+        ForecastRequestDto request1 = new ForecastRequestDto("London");
+        forecastService.get7DayForecast(request1);
+        ForecastRequestDto request2 = new ForecastRequestDto("Tomsk");
+        forecastService.get7DayForecast(request2);
     }
 
     @Test
     @Transactional
     @WithMockUser(username = "user1@company.com", roles = "ADMIN")
-    void getAllCitiesInFavorites() {
-
+    void getWarmestCity() {
 
         User checkCity = userService.getCurrentUser();
 
-        cityService.addCityToFavorite("Berlin");
 
-        Set<CityResponseDto> favorites = statisticService.getAllCitiesInFavorites();
-
-
-        assertNotNull(favorites);
-        assertTrue(favorites.stream().anyMatch(city -> city.getName().equals("Berlin")));
+        CityResponseDto city = statisticService.getWarmestCity();
 
 
-        assertTrue(checkCity.getCities().stream().anyMatch(c -> "Berlin".equals(c.getName())));
+        assertNotNull(city);
+        assertTrue(city.getName().equals("London"));
 
 
     }
+    @Test
+    @Transactional
+    @WithMockUser(username = "user1@company.com", roles = "ADMIN")
+    void warmestCityNotFoundIfDatabaseIsEmpty() {
 
+        User user = userService.getCurrentUser();
 
+        forecastRepository.deleteAll();
+
+        assertThrows(NotFoundException.class,
+                () -> statisticService.getWarmestCity());
+    }
 }
